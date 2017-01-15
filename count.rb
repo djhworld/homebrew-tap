ea1ec5b36e671c4fb7080f5b4a92b1f9096b0085
djhworld <djharperuk@gmail.com>
2017-01-15 21:34:38 +0000

    Updating version of count

--- count.rb
+++ count.rb
@@ -1,8 +1,8 @@
 class Count < Formula
   desc "total unique entries in line delimited input from stdin"
   homepage "https://github.com/djhworld/count"
-  url "https://github.com/djhworld/count/archive/0.0.3.tar.gz"
-  sha256 "4c46960b78efe7903199b7a49c97a87e73d6ff9366203eb13983b6ae4988d78f"
+  url "https://github.com/djhworld/count/archive/0.0.4.tar.gz"
+  sha256 "2175be88fc93a62caa7f861b419aaeadef7bb0267a928d73c1ab5664aa0da6dc"
   head "https://github.com/djhworld/count.git"
 
   depends_on "go" => :build
