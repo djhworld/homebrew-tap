3aa3f5724945495b0e59860336d071e2e74c9b67
djhworld <djharperuk@gmail.com>
2017-01-21 20:54:12 +0000

    stats.rb

--- stats.rb
+++ stats.rb
@@ -1,18 +1,19 @@
 class Stats < Formula
   desc "outputs statistical information about line delimited numbers from stdin"
   homepage "https://github.com/djhworld/stats"
-  url "https://github.com/djhworld/stats/archive/0.0.1.tar.gz"
-  sha256 "da817f4797025fb7a863b50b0d0a23d3d3887e4cd25485654cf0debd1f7e7079"
+  url "https://github.com/djhworld/stats/archive/0.0.2.tar.gz"
+  sha256 "115489871bfcc9de624547f645f2e2764ae3dd48618c28a25c34de12329471ba"
   head "https://github.com/djhworld/stats.git"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOBIN"] = bin
-    ENV["GOPATH"] = buildpath/"vendor"
-    ENV["GOHOME"] = buildpath
+    #ENV["GOBIN"] = bin
+    #ENV["GOPATH"] = buildpath/"vendor"
+    #ENV["GOHOME"] = buildpath
 
-    system "go", "build", "-o", bin/"stats", "src/github.com/djhworld/stats/stats.go"
+    system "make", "setup"
+    system "make"
   end
 
   test do
