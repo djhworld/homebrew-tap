9d93aa8c74a2e697f570fca18086edc1e4468eab
djhworld <djharperuk@gmail.com>
2017-02-10 14:32:52 +0000

    Updated to 0.0.3

--- stats.rb
+++ stats.rb
@@ -1,8 +1,8 @@
 class Stats < Formula
   desc "outputs statistical information about line delimited numbers from stdin"
   homepage "https://github.com/djhworld/stats"
-  url "https://github.com/djhworld/stats/archive/0.0.2.tar.gz"
-  sha256 "115489871bfcc9de624547f645f2e2764ae3dd48618c28a25c34de12329471ba"
+  url "https://github.com/djhworld/stats/archive/0.0.3.tar.gz"
+  sha256 "a208a7c04f5771591290c081041b3567b6a464e74109aecce7992675efc5cdf3"
   head "https://github.com/djhworld/stats.git"
 
   depends_on "go" => :build
